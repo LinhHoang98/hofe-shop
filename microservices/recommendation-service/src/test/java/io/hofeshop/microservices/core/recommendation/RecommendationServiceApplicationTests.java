e9fee070775becba7abf1966ca9548ccc2cbae62
LinhHN <linhhoang981221@gmail.com>
2025-06-02 21:01:38 +0700

    Initial commit

---
+++ microservices/recommendation-service/src/test/java/io/hofeshop/microservices/core/recommendation/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hofeshop.microservices.core.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
